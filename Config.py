c4f643d6d74b134ca666098ff36abc3089fa0072
imtiaz <imtiaz@gmail.com>
2026-05-10 20:24:18 -0400

    First commit

--- Config.py
+++ Config.py
@@ -1,3 +1,4 @@
 This is Config File
 
 This is new line
+This is first commit for Config
--- menu.sh
+++ menu.sh
@@ -4,3 +4,5 @@ This is second line of menu.sh
 This is my new line
 
 This is new line on 05/10
+
+This is first commit for menu
